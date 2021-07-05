2613ef8597321f06a5dff40c25aef5920bc36206
DONGVK <57867631+DONGVK@users.noreply.github.com>
2021-07-05 11:45:37 +0200

    Page vote

--- Dianalysis-main/client/components/Profile.vue
+++ Dianalysis-main/client/components/Profile.vue
@@ -1,58 +1,14 @@
 <template>
   <section>
     <navbar :connected="connected" @log-out="logOut"></navbar>
-    <div class="profile_container">
-      <form id="form_profile">
-      <h2 class="title_container">Vos informations</h2>
-    <div class="collum">
-      <div class="collum1">
-        <div class="data_profile">
-          <h6>Mon prénom&nbsp;:</h6>
-        <input type="text" id="prenom" name="user_prenom" placeholder="Prénom" class="input_style"/>
-        </div>
-        
-        <div class="data_profile">
-        <h6>Mon nom&nbsp;:</h6>
-        <input type="text" id="nom" name="user_nom" placeholder="Nom" class="input_style"/>
-        </div>
-
-        <div class="data_profile">
-        <h6>Mon adresse e-mail&nbsp;:</h6>
-        <input id="email" name="email_message" placeholder=" E-mail" class="input_style"/>
-        </div>
-
-        <div class="data_profile">
-        <h6>Pseudo&nbsp;:</h6>
-        <input type="text" name="username" placeholder="Identifiant utilisateur" class="input_style"/>
-        </div>
-      </div>
-      <div class="collum2">
-        <div class="data_profile">
-        <h6>Mot de passe actuel&nbsp;:</h6>
-        <input type="password" name="actual_password" placeholder="Mot de passe" class="input_style"/>
-        </div>
-
-        <div class="data_profile">
-        <h6>Nouveau mot de passe&nbsp;:</h6>
-        <input type="password" name="new_mdp" placeholder="Saisissez votre nouveau mot de passe" class="input_style" id="password"/>
-        </div>
-
-        <div class="data_profile">
-        <h6>Confirmation du mot de passe&nbsp;:</h6>
-        <input type="text" name="new_mdp2" placeholder="Saisissez votre nouveau mot de passe" class="input_style" />
-        </div>
-      </div>
-
-      <div class="data_profile">
-        <h6>Taux de glucose journalier en g&nbsp;:</h6>
-        <input type="number" name="tauxGlucose" placeholder="Taux de glucose" class="input_style" id="glucose"/>
-        </div>
-
-    </div>
-        <button type="submit" class="button_style">Valider les modifications</button>
-      </form>
-    </div>
-    <p id="errorModifyUserMessage"></p>
+    <h2> Vos informations : </h2>
+    <ul>
+      <li><img :src="user[8]"/></li>
+      <li>Nom : {{user[1]}} </li>
+      <li>Prénom : {{user[2]}} </li>
+      <li> Date de naissance : {{this.date()}} </li>
+      <li>Adresse mail : {{user[4]}} </li>
+    </ul>
   </section>
 </template>
 
@@ -60,6 +16,7 @@
 const Navbar = window.httpVueLoader("./components/Navbar.vue");
 module.exports = {
   props: {
+    user: { type: Array, default: [] },
     connected: { type: Boolean },
   },
   components: {
@@ -67,145 +24,46 @@ module.exports = {
   },
   data() {
     return {
-      modifiedUser: {
-        username: "",
-        email: "",
-        password: "",
-        firstname: "",
-        lastname: "",
-        phone: "",
-      },
     };
   },
   methods: {
     logOut() {
       this.$emit("log-out");
     },
-    updateUser() {
-      if (
-        document.getElementById("password").value ==
-        document.getElementById("confirmationPassword").value
-      ) {
-        this.$emit("update-user", this.modifiedUser);
-      } else {
-        document.getElementById("errorModifyUserMessage").innerHTML =
-          "Mot de passe non identique.";
-      }
-    },
+    date(){
+      var l = this.user[3].split("-");
+      var date = l[2] + "/" + l[1] + "/" + l[0]
+      return date
+    }
   },
 };
 </script>
 
 <style scoped>
-
-.collum1{
-  padding-top:20px;
-  width: 400px;
-}
-.collum2{
-  padding-top:20px
-}
-.input_style{
-  border-color:rgb(73, 89, 231);
-  border-radius: 10px;
-}
-
-.data_profile{
-  color:white;
-  margin: 50px;
-}
-
-.profile_container{
-  background-image: url("../img/fondbleu.jpg");
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-}
-
-#form_profile{
-  background: linear-gradient(#071e38, #040614);
-  flex-direction: column;
-  margin: auto auto;
-  flex-wrap: wrap;
-  align-items: center;
-  padding: 60px;
-  width: 100%;
-  height: clac(100vh - 200px);
-}
-
-.collum{
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-}
-
-
-div#first {
-  background-color: rgb(141, 128, 128);
-}
-div#first div {
-  display: inline-block;
-  width: 48%;
-}
-div#first div.gauche {
-  background-color: rgb(185, 178, 178);
-}
-div#first div.droite {
-  vertical-align: top;
-}
-
-div#first div h6,
-h3,
-input {
-  
-  margin-bottom: 10px;
-  width: 300px;
-}
-
-#vos_informations{
-  font-size:50px;
-  color:white;
-  margin: auto;
+h2{
+  display: block;
+  color : white;
   text-align: center;
 }
 
-#errorModifyUserMessage{
-  margin: 0;
+ul{
+  display: block;
+  text-align: center;
+  list-style-type:none
+}
+li img{
+  height: 200px;
+  width: 200px;
+  border-radius: 50%;
+  margin: auto;
+  margin-bottom: 1%;
 }
 
-.titre_centre{
-    text-align: center;
+section{
+  background-image: linear-gradient(to right top, #c33764, #a62a6f, #822675, #572676, #1d2671);
+  align-items: center;
+  font-family: "Montserrat", sans-serif;
+  height: 100%;
 }
 
-.title_container {
-    background: rgb(1,31,95);
-    background: linear-gradient(90deg, #071e38 0%, rgb(2, 2, 146)  46%, #071e38 100%);
-    font-size: 45px;
-    padding-top: 20px;
-    color: white;
-    text-align: center;
-    justify-content: center;
-    margin: auto;
-    height: 110px;
-    margin: 0;
-}
-
-section {
-background: linear-gradient(#071e38, #040614);
-}
-
-.button_style{
-    color:rgb(255, 255, 255);
-    border-color: rgb(0, 0, 0);
-    background-color: rgb(27, 44, 194);
-    border-radius:10px;
-    display: block;
-    margin: auto auto;
-    height: 2em;
-    margin-top: 10px;
-  }
-
-#errorSampleMessage{
-    margin: 0;
-}
 </style>
---
+++ Dianalysis-main/client/components/Vote.vue
@@ -0,0 +1,172 @@
+<template>
+  <section>
+    <navbar :connected="connected" @log-out="logOut"></navbar>
+    
+    <h2 class="title_container">Liste des candidats</h2>
+     <div class="container">
+      <div id="choices">
+        <div v-for="(candidat, index) in candidats" :key="index" class="item" :id="index">
+          <img :src="candidat[8]"/>
+          </br>
+          <label class="name"> {{ candidat[1] }} - {{ candidat[2] }}</label>
+          </br>
+          <label class="politique"> {{ candidat[9] }}</label>
+          </br>
+          <button id="index">Voter</button>
+        </div>
+      </div>
+    </div>
+
+    <div id="dia-fleches">
+		  <span id="dia-gauche" v-on:click="move()" >&lt;</span>
+		  <span id="dia-droite" v-on:click="move()">&gt;</span>
+	  </div>
+
+  </section>
+</template>
+
+<script>
+const Navbar = window.httpVueLoader("./components/Navbar.vue");
+module.exports = {
+  props: {
+    candidats : {type: Array, default: []},
+    connected: { type: Boolean },
+  },
+  components: {
+    Navbar,
+  },
+  methods: {
+    logOut() {
+      this.$emit("log-out");
+    },
+    move(){
+      var hdiv = parseInt($('div.item').height());
+      $("#dia-fleches span").click(function(){
+        if( $(this).attr('id') == "dia-gauche" ){
+          var gauche = parseInt($('div#choices').css('top'));
+          gauche += hdiv;
+          if( gauche <= 0 && gauche%hdiv == 0){
+            $('div#choices').animate({ top: gauche });
+          }
+        }else{
+          var droite = parseInt($('div#choices').css('top'));
+          droite -= hdiv;
+          console.log(droite);
+          if( droite > -hdiv*6 && droite%hdiv == 0 ){
+            $('div#choices').animate({ top: droite });
+          }
+        }
+      });
+    },
+  },
+};
+</script>
+<style scoped>
+h2.title_container{
+  display: block;
+  color : white;
+  text-align: center;
+}
+
+/************************************************
+* ITEM
+************************************************/ 
+ 
+div.container{
+    margin: auto;
+    height: 60vh;
+    width: 100vw;
+    display: block;
+    clear: both;
+    overflow: hidden;
+    text-align: center;
+    position: relative;
+}
+
+div#choices{
+  position: relative;
+  height: 100vh;
+}
+
+.item{
+    margin-left: 2%;
+    width: 100%;
+    height: 60%;
+    background-color:white;
+    border-radius: 10px;
+    color: black;
+    text-align: center;
+}
+
+.item:last-child{
+  top:50%;
+}
+
+.item img{
+  margin-left: 5%;
+  height: 60vh;
+  width: 30vw;
+  border-radius: 50%;
+  object-fit: cover;
+  float: left;
+  left: -20%;
+}
+
+.name{
+  margin-top: 10%;
+}
+
+/* Commun aux deux flêches */
+#dia-fleches span
+{
+	position: absolute;
+	top: 200px;
+	font-size: 30px;
+	font-family: sans;
+	background-color: rgba(0,0,0,.4);
+	color: #bbb;
+	padding: .1em;
+	/* Le pointeur de la souris */
+	cursor: pointer;
+}
+#dia-gauche
+{
+	left: 0;
+  margin-top: 5%;
+}
+#dia-droite
+{
+	right: 0;
+  margin-top: 5%;
+}
+#dia-fleches span:hover
+{
+	color: #fff;
+}
+
+
+section{
+  background-image: linear-gradient(to right top, #c33764, #a62a6f, #822675, #572676, #1d2671);
+  align-items: center;
+  font-family: "Montserrat", sans-serif;
+  height: 100%;
+}
+
+button {
+  background-color: #2a3088;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 30%;
+  border-radius: 10px;
+  border: 1px solid #000000;
+}
+
+button:hover {
+  background-color: white;
+  color: #000000;
+  border: 1px solid #000000;
+}
+</style>
